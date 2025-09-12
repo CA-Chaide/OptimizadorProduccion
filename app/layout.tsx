a4204955a53daf1b5d7c7a600e455b81b29646d0
Your Name <erick.gordillo@chaideychaide.com>
2025-09-12 18:24:48 +0000

    el favicon.ico ya lo puse en: src/app/favicon.ico
    PERO NO LO VEO!

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,6 @@ const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 export const metadata: Metadata = {
   title: 'Planificador Produccion',
   description: 'Application for optimizing production planning.',
-  icons: {
-    icon: '/favicon.ico',
-  },
 };
 
 export default function RootLayout({
