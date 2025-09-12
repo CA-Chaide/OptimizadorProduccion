fb29e8436316ebee7c656b4049001ced63ac07a8
Your Name <erick.gordillo@chaideychaide.com>
2025-09-12 18:20:39 +0000

    cambia el nombre de la aplicacion para ver en la pestana como: Planifica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { cn } from '@/lib/utils';
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
-  title: 'Production Optimizer Next',
+  title: 'Planificador Produccion',
   description: 'Application for optimizing production planning.',
   icons: {
     icon: '/favicon.ico',
