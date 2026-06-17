a20667f8a657bd44d489e466d5fd4320c7a5fd7e
Sebastian Guerrero <sebastian.guerrero@chaideychaide.com>
2026-06-17 18:40:19 +0000

    Por favor elimina la pestala "Mat.Brutos" de "Planificación Táctica Mueb

--- src/components/TacticalPlanMueblesSection.tsx
+++ src/components/TacticalPlanMueblesSection.tsx
@@ -12,7 +12,6 @@ import { OrdenesFertTabSection } from './OrdenesFertTabSection';
 import { TiemposEnsambladoTab } from './TiemposEnsambladoTab';
 import { CuboInventariosTab } from './CuboInventariosTab';
 import { CuboInventariosTelasTab } from './CuboInventariosTelasTab';
-import { MaterialesBrutosTab } from './MaterialesBrutosTab';
 import { HabilidadesMueblesTab } from './HabilidadesMueblesTab';
 import { grupoService } from '@/services/grupo.service';
 import { restriccionService } from '@/services/restriccion.service';
@@ -218,7 +217,6 @@ export const TacticalPlanMueblesSection: React.FC = () => {
               <TabsTrigger value="habilidades" className="text-sm py-2 px-1">Habilidades</TabsTrigger>
               <TabsTrigger value="cascos" className="text-sm py-2 px-1">Cascos</TabsTrigger>
               <TabsTrigger value="telas" className="text-sm py-2 px-1">Telas</TabsTrigger>
-              <TabsTrigger value="materialesBrutos" className="text-sm py-2 px-1">Mat. Brutos</TabsTrigger>
               <TabsTrigger value="plan" className="text-sm py-2 px-1 font-bold">PLAN</TabsTrigger>
           </TabsList>
           
@@ -296,19 +294,6 @@ export const TacticalPlanMueblesSection: React.FC = () => {
                   </CardHeader>
                   <CardContent>
                       <CuboInventariosTelasTab />
-                  </CardContent>
-              </Card>
-          </TabsContent>
-          <TabsContent value="materialesBrutos" className="mt-4">
-              <Card>
-                  <CardHeader>
-                      <CardTitle>Explosión de Materiales Brutos</CardTitle>
-                      <CardDescription>
-                          Consumo de materias primas por producto terminado. Relacione el código de material del PLAN con esta tabla para saber qué insumos requiere.
-                      </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                      <MaterialesBrutosTab />
                   </CardContent>
               </Card>
           </TabsContent>
